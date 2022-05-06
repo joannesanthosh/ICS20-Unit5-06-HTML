ed8b4536574043ef1d28d071b257d42752b2a6e1
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-06 17:28:26 +0000

    change 2 files

--- js/script.js
+++ js/script.js
@@ -9,22 +9,20 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-Unit6-01-PWA/sw.js", {
-    scope: "/ICS20-Unit6-01-PWA/",
+  navigator.serviceWorker.register("/ICS20-Unit5-06-HTML/sw.js", {
+    scope: "/ICS20-Unit5-06-HTML/",
   })
 }
 
 /**
- * This function converts the degrees from fahrenheit to celsius
+ * This function multiplies numbers using loops
  */
 function convert() {
   // input
-  const fahrenheit = parseInt(document.getElementById("fahrenheit").value)
+  const firstNumber = parseInt(document.getElementById("first-number").value)
+  const secondNumber = parseInt(document.getElementById("second-number").value)
 
   // process
-  const celsius = ((fahrenheit - 32) * 5) / 9
+  while
 
-  // output
-  document.getElementById("celsius").innerHTML =
-    "<p>The  temperature in celsius is: " + celsius.toFixed(2) + "°<p>"
 }
